74f2326a2e5d3317841437462880856928eb0515
Maghnaoui <maghnaoui.dev@gmail.com>
2025-03-27 03:34:58 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -25,7 +25,8 @@ async function fetchJournalsForYear(year) {
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
             "Accept-Language": "ar,en-US;q=0.9,en;q=0.8",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br"
         }
     });
     
